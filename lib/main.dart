1da3b5a9178aad6eeb7f774798727f0030992862
gosteve <nomuojamaro@gmail.com>
2020-04-29 12:19:56 +0100

    imported the correct audioplayer package created an instance of the audio player in the flatbutton added 7 buttons to play 7 different sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -10,7 +10,87 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                child: Container(
+                  width: 100,
+                  height: 30,
+                  color: Colors.red,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+              ),
+              FlatButton(
+                child: Container(
+                  width: 100,
+                  height: 30,
+                  color: Colors.deepOrange,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+              ),
+              FlatButton(
+                child: Container(
+                  width: 100,
+                  height: 30,
+                  color: Colors.yellow,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+              ),
+              FlatButton(
+                child: Container(
+                  width: 100,
+                  height: 30,
+                  color: Colors.green,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+              ),
+              FlatButton(
+                child: Container(
+                  width: 100,
+                  height: 30,
+                  color: Colors.blueGrey,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+              ),
+              FlatButton(
+                child: Container(
+                  width: 100,
+                  height: 30,
+                  color: Colors.blue,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+              ),
+              FlatButton(
+                child: Container(
+                  width: 100,
+                  height: 30,
+                  color: Colors.purple,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
